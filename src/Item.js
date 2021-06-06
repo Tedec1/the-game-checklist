f1b5efacdd62d5c15df9daf93fb07c04ae3290e9
Tedec1 <jaspermesenbrink@gmail.com>
2021-06-06 15:26:08 -0600

    formatting

--- src/Item.js
+++ src/Item.js
@@ -1,10 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import { useState } from "react";
 
-const Item = ({num}) => {
-    const [active, setActive] = useState(false)
-    
-    return (<div style={{margin:"7px"}}>
-<span style={{textDecoration: active ? "line-through" : "none"}}>{" "}{num}{" "}</span>
-</div>)}
+const Item = ({ num }) => {
+	const [active, setActive] = useState(false);
+
+	return (
+		<div style={{ margin: "7px" }}>
+			<span style={{ textDecoration: active ? "line-through" : "none" }}>
+				{" "}
+				{num}{" "}
+			</span>
+		</div>
+	);
+};
 
 export default Item;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
-import {default as Item} from "./Item";
-import React from 'react';
-import './index.css'
-import ReactDom from 'react-dom';
+import { default as Item } from "./Item";
+import React from "react";
+import "./index.css";
+import ReactDom from "react-dom";
 const App = () => {
-    const l = []
-    for (let i = 1; i < 101; i++) {
-        l.push(<Item num={i}/>)
-    }
-    return (<div className="list">
-        {
-            l
-        }
-    </div>
-    )
-}
+	const l = [];
+	for (let i = 1; i < 101; i++) {
+		l.push(<Item num={i} />);
+	}
+	return <div className="list">{l}</div>;
+};
 
-const numpad = () => {
-    
-}
+const numpad = () => {};
 
-ReactDom.render(<App/>,document.getElementById('root'))
+ReactDom.render(<App />, document.getElementById("root"));
